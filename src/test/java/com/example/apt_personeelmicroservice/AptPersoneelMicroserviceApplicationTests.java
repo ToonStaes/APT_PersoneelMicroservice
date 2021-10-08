e1ff5fd2a8986a2592dcecb26408425327f07637
Arne Hus <r0793284@student.thomasmore.be>
2021-10-08 13:49:44 +0200

    Inital structure

---
+++ src/test/java/com/example/apt_personeelmicroservice/AptPersoneelMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apt_personeelmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptPersoneelMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
