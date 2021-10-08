77bf9e3e9e7a469418d9c6e168f58bda343f3e5e
Arne Hus <r0793284@student.thomasmore.be>
2021-10-08 13:49:44 +0200

    Initial structure

---
+++ src/main/java/com/example/apt_personeelmicroservice/AptPersoneelMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.apt_personeelmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptPersoneelMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptPersoneelMicroserviceApplication.class, args);
+    }
+
+}
